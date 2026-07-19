92974f23784a996ea1351924e069a34005948020
Eric Beam <eric.beam622@gmail.com>
2026-07-19 00:26:54 -0400

    Implement countdown timer and game over state

--- game/dungeon-delivery/scenes/gameManager.gd
+++ game/dungeon-delivery/scenes/gameManager.gd
@@ -1,10 +1,15 @@
 extends Node
 
 var score := 0
+var game_over := false
+var time_remaining := 60
 
 @onready var score_label = $"../UI/ScoreLabel"
 
 @onready var status_label = $"../UI/StatusLabel"
+
+@onready var timer_label = $"../UI/TimerLabel"
+@onready var game_timer = $"../GameTimer"
 
 @onready var main_scene = get_parent()
 
@@ -14,9 +19,33 @@ func _ready():
 
 	update_package_status(false)
 
+	timer_label.text = "Time: " + str(time_remaining)
+
 	spawn_package()
 
+	game_timer.timeout.connect(_on_timer_timeout)
+
+func _on_timer_timeout():
+
+	time_remaining -= 1
+
+	timer_label.text = "Time: " + str(time_remaining)
+
+	if time_remaining <= 0:
+
+		game_over = true
+
+		game_timer.stop()
+
+		timer_label.text = "GAME OVER"
+
+		print("Game Over")
+
 func add_score(amount):
+
+	if game_over:
+		return
+
 	score += amount
 
 	score_label.text = "Score: " + str(score)
